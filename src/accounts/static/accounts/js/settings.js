bec9ab922894d574e4c4d155e612d080e3335fe9
user <you@example.com>
2021-10-01 19:21:08 +0300

    rename src to app

--- src/accounts/static/accounts/js/settings.js
+++
@@ -1,3 +0,0 @@
-$('input[name=user-image]').change(function(ev) {
-    document.getElementById('user-image-form').submit();
-});
--- src/src/context_processors.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings # import the settings file
-
-def project_name(request):
-    # return the value you want as a dictionnary. you may add multiple values in there.
-    return {'PROJECT_NAME': settings.PROJECT_NAME}
