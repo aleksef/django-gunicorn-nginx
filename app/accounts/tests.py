1183b6e245b7105e4000a8bcb21ff39a187fae0a
aleksef <mk15ns@protonmail.com>
2021-11-04 12:46:18 +0300

    add create account test

--- app/accounts/tests.py
+++ app/accounts/tests.py
@@ -3,7 +3,7 @@ from django.test import Client
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django import forms
-from .forms import LoginForm
+from .forms import LoginForm, CreateForm
 from urllib.parse import urlencode
 
 
@@ -30,3 +30,14 @@ class AccountsTest(TestCase):
         response = self.client.post('/accounts/login/', form_data, content_type="application/x-www-form-urlencoded")
         # Check if client is logged in
         self.assertEquals(response.url, '/accounts/settings/') # client can access settings
+    
+    def test_can_create_account(self):
+        form_data = {'username': 'alice',
+                     'email': 'alice@gmail.com',
+                     'password': 'password',
+                     'confirm_password': 'password'}
+        response = self.client.post('/accounts/create/',
+                                    urlencode(form_data),
+                                    content_type="application/x-www-form-urlencoded")
+        self.assertEquals(response.url, '/accounts/settings/') # client can access settings
+        self.assertTrue(User.objects.filter(username='alice').exists()) # user exists
